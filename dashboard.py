3807c4b94bd0cb24d1f0577d083fcc2b098dbcc5
Dan Joshua Santivañez Gutarra <83432705+djsg2021utec@users.noreply.github.com>
2024-09-09 03:15:01 -0500

    Actualización mejorada y puertos abiertos

--- dashboard.py
+++ dashboard.py
@@ -350,6 +350,6 @@ def update_bar_chart(selected_experiments, selected_metric, dark_mode):
     return bar_fig
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-    #port = int(os.environ.get('PORT', 8050))
-    #app.run_server(host='0.0.0.0', port=port, debug=True)
+    #app.run_server(debug=True)
+    port = int(os.environ.get('PORT', 8050))
+    app.run_server(host='0.0.0.0', port=port, debug=True)
